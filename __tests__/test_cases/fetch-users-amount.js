f24afc532afa20b0e220dce6984db167b95ab692
Kevin Wang <rocwang0316@gmail.com>
2019-02-21 12:45:06 -0800

    Fixed the describe name.

--- __tests__/test_cases/fetch-users-amount.js
+++ __tests__/test_cases/fetch-users-amount.js
@@ -3,7 +3,7 @@ import initEvns from '../helpers/InitialEnvs';
 
 let context;
 
-describe('fetch-all-user-list: invoke the Get / endpoint', () => {
+describe('fetch-users-amount: invoke the Get / endpoint', () => {
   beforeAll(async () => {
     jest.setTimeout(10000); // Setup a longer timeout to allow fetching the credantial keys from SSM.
     await initEvns();
