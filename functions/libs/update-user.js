c9ec998f128989551b207ed366b580abac90e3ce
Kevin Wang <rocwang0316@gmail.com>
2019-02-22 15:04:10 -0800

    Added a update-user library.

---
+++ functions/libs/update-user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { ObjectId } = require('mongodb');
+const cloudwatch = require('@kevinwang0316/cloudwatch');
+const { promiseReturnResult } = require('@kevinwang0316/mongodb-helper');
+
+module.exports.handler = async (userId, user, removeFields) => cloudwatch.trackExecTime(
+  'MongoDBFindAndUpdateLatancy',
+  () => promiseReturnResult(db => db
+    .collection(process.env.userCollectionName).findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      removeFields ? { $set: user, $unset: removeFields } : { $set: user },
+      { returnOriginal: false, projection: { pushSubscription: 0 } },
+    )),
+);
